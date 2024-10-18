917134ee8c09ac9fc631faa6f81b26ca992d898a
Sota Noniwa <surfersou@gmail.com>
2024-10-19 08:38:16 +0900

    Iterator

---
+++ Iterator/src/main/java/com/iterator/ThemeColor.java
@@ -0,0 +1,30 @@
+package com.iterator;
+
+// This enum represents the aggregate from iterator pattern
+public enum ThemeColor {
+    
+    RED,
+    ORANGE,
+    BLACK,
+    WHITE;
+
+    public static Iterator<ThemeColor> getIterator() {
+        return new ThemeColorIterator();
+    }
+
+    // Concrete iterator as inner class
+    private static class ThemeColorIterator implements Iterator<ThemeColor> {
+
+        private int position;
+
+        @Override
+        public ThemeColor next() {
+            return ThemeColor.values()[position++];
+        }
+
+        @Override
+        public boolean hasNext() {
+            return position < ThemeColor.values().length;
+        }
+    }
+}
